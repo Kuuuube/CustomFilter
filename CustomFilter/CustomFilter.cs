fda616f41109b7bba3f8d861d35c8323334d1950
Sina Kamali <sina.kamali76@gmail.com>
2023-07-12 11:45:51 +0330

    Fixed last values not saving
    - added a timeout to optionally reset last value
    - minor refactoring of code for better readability

--- CustomFilter/CustomFilter.cs
+++ CustomFilter/CustomFilter.cs
@@ -1,18 +1,48 @@
 ﻿using System.Numerics;
 using AngouriMath;
 using AngouriMath.Core;
-using AngouriMath.Extensions;
 using OpenTabletDriver.Plugin;
 using OpenTabletDriver.Plugin.Attributes;
 using OpenTabletDriver.Plugin.DependencyInjection;
 using OpenTabletDriver.Plugin.Output;
 using OpenTabletDriver.Plugin.Tablet;
+using OpenTabletDriver.Plugin.Timing;
 
 namespace CustomFilter;
 
 [PluginName("Custom Filter")]
 public class CustomFilter : IPositionedPipelineElement<IDeviceReport>
 {
+    [Property("Reset Time (ms)"), DefaultPropertyValue(-1), ToolTip(
+         "Time in milliseconds between reports until last values are cleared\n" +
+         "- Negative value means never reset\n" +
+         "- Zero means always reset\n")]
+    public int ResetTime { get; set; }
+
+    [Property("X coordinate polynomial"), DefaultPropertyValue("x"), ToolTip(
+         "A polynomial that calculates the X coordinate\n" + TOOLTIP)]
+    public string XFunc { get; set; }
+
+    [Property("Y coordinate polynomial"), DefaultPropertyValue("y"), ToolTip(
+         "A polynomial that calculates the Y coordinate\n" + TOOLTIP)]
+    public string YFunc { get; set; }
+
+    [Property("Pressure polynomial"), DefaultPropertyValue("p"), ToolTip(
+         "A polynomial that calculates the pressure\n" + TOOLTIP)]
+    public string PFunc { get; set; }
+
+    [Property("X tilt polynomial"), DefaultPropertyValue("tx"), ToolTip(
+         "A polynomial that calculates the X tilt\n" + TOOLTIP)]
+    public string TXFunc { get; set; }
+
+    [Property("Y tilt polynomial"), DefaultPropertyValue("ty"), ToolTip(
+         "A polynomial that calculates the Y tilt\n" + TOOLTIP)]
+    public string TYFunc { get; set; }
+    
+    private TimeSpan ResetTimeSpan;
+    
+    private HPETDeltaStopwatch stopwatch = new HPETDeltaStopwatch(false);
+    
     private const string TOOLTIP =  "x = The X coordinate\n" +
                                     "y = The Y coordinate\n" +
                                     "p = The pressure\n" +
@@ -34,18 +64,21 @@ public class CustomFilter : IPositionedPipelineElement<IDeviceReport>
 
     private readonly string[] variables = { "x", "y", "p", "tx", "ty", "d", "lx", "ly", "lp", "ltx", "lty", "ld", "mx", "my", "mp", "cx", "cy", "cp" };
 
-    public FastExpression? CalcX = null;
-    public FastExpression? CalcY = null;
-    public FastExpression? CalcP = null;
-    public FastExpression? CalcTX = null;
-    public FastExpression? CalcTY = null;
+    private FastExpression? CalcX = null;
+    private FastExpression? CalcY = null;
+    private FastExpression? CalcP = null;
+    private FastExpression? CalcTX = null;
+    private FastExpression? CalcTY = null;
 
-    public Vector2 LastPos = Vector2.Zero;
-    public uint LastP = 0;
-    public Vector2 LastT = Vector2.Zero;
-    public uint LastD = 0;
-    public Vector2 LastComputedPos = Vector2.Zero;
-    public uint LastComputedPressure = 0;
+    private Vector2 LastPos = Vector2.Zero;
+    private uint LastP = 0;
+    private Vector2 LastT = Vector2.Zero;
+    private uint LastD = 0;
+    private Vector2 LastComputedPos = Vector2.Zero;
+    private uint LastComputedPressure = 0;
+    
+    [TabletReference]
+    public TabletReference TabletReference { get; set; }
 
     /// <summary>
     /// Recompiles the X and Y polynomials to a function.
@@ -53,6 +86,9 @@ public class CustomFilter : IPositionedPipelineElement<IDeviceReport>
     [OnDependencyLoad]
     public void Recompile()
     {
+        stopwatch.Reset();
+        ResetTimeSpan = new TimeSpan(0, 0, 0, 0, ResetTime);
+        
         Entity xExpr = XFunc;
         Entity yExpr = YFunc;
         Entity pExpr = PFunc;
@@ -118,6 +154,10 @@ public class CustomFilter : IPositionedPipelineElement<IDeviceReport>
 
     public void Consume(IDeviceReport value)
     {
+        if (value is OutOfRangeReport)
+            return;
+
+        // read input values
         var digitizer = TabletReference.Properties.Specifications.Digitizer;
         var pen = TabletReference.Properties.Specifications.Pen;
 
@@ -126,22 +166,38 @@ public class CustomFilter : IPositionedPipelineElement<IDeviceReport>
         Vector2 tilt = Vector2.Zero;
         uint distance = 0;
 
-        if (value is ITiltReport r1)
+        if (value is ITabletReport r1)
         {
-            tilt = r1.Tilt;
+            pos = r1.Position;
+            pressure = r1.Pressure;
+        }
+        
+        if (value is ITiltReport r2)
+        {
+            tilt = r2.Tilt;
         }
 
-        if (value is IProximityReport r2)
+        if (value is IProximityReport r3)
         {
-            distance = r2.HoverDistance;
+            distance = r3.HoverDistance;
+        }
+        
+        // check for last value reset timeout
+        var deltaTime = stopwatch.Restart();
+        if (ResetTime >= 0 && deltaTime >= ResetTimeSpan)
+        {
+            LastPos = pos;
+            LastP = pressure;
+            LastT = tilt;
+            LastD = distance;
+            LastComputedPos = Vector2.Zero;
+            LastComputedPressure = 0;
         }
 
+        // evaluate value expressions
         if (value is ITabletReport report)
         {
             //Compiled expressions return a Complex, so we need to downcast it
-            pos = report.Position;
-            pressure = report.Pressure;
-
             if (CalcX != null)
                 pos.X = (float)CalcX.Call(report.Position.X, report.Position.Y, report.Pressure, tilt.X, tilt.Y, distance, LastPos.X, LastPos.Y, LastP, LastT.X, LastT.Y, LastD, digitizer.MaxX, digitizer.MaxY, pen.MaxPressure, LastComputedPos.X, LastComputedPos.Y, LastComputedPressure).Real;
 
@@ -151,13 +207,17 @@ public class CustomFilter : IPositionedPipelineElement<IDeviceReport>
             if (CalcP != null)
                 pressure = (uint)CalcP.Call(report.Position.X, report.Position.Y, report.Pressure, tilt.X, tilt.Y, distance, LastPos.X, LastPos.Y, LastP, LastT.X, LastT.Y, LastD, digitizer.MaxX, digitizer.MaxY, pen.MaxPressure, LastComputedPos.X, LastComputedPos.Y, LastComputedPressure).Real;
 
-            report.Pressure = pressure;
+            LastPos = report.Position;
+            LastP = report.Pressure;
             report.Position = pos;
+            report.Pressure = pressure;
+            LastComputedPos = pos;
+            LastComputedPressure = pressure;
 
             value = report;
         }
 
-        if (value is ITiltReport r3)
+        if (value is ITiltReport r4)
         {
             if (CalcTX != null)
                 tilt.X = (float)CalcTX.Call(pos.X, pos.Y, pressure, tilt.X, tilt.Y, distance, LastPos.X, LastPos.Y, LastP, LastT.X, LastT.Y, LastD, digitizer.MaxX, digitizer.MaxY, pen.MaxPressure, LastComputedPos.X, LastComputedPos.Y, LastComputedPressure).Real;
@@ -165,45 +225,24 @@ public class CustomFilter : IPositionedPipelineElement<IDeviceReport>
             if (CalcTY != null)
                 tilt.Y = (float)CalcTY.Call(pos.X, pos.Y, pressure, tilt.X, tilt.Y, distance, LastPos.X, LastPos.Y, LastP, LastT.X, LastT.Y, LastD, digitizer.MaxX, digitizer.MaxY, pen.MaxPressure, LastComputedPos.X, LastComputedPos.Y, LastComputedPressure).Real;
 
-            r3.Tilt = tilt;
-            value = r3;
-        }
-
-        if (value is IProximityReport r4)
-        {
-            r4.HoverDistance = distance;
+            LastT = r4.Tilt;
+            r4.Tilt = tilt;
+            
             value = r4;
         }
 
-        Emit?.Invoke(value);
+        if (value is IProximityReport r5)
+        {
+            LastD = r5.HoverDistance;
+            r5.HoverDistance = distance;
+            
+            value = r5;
+        }
 
-        LastComputedPos = pos;
-        LastComputedPressure = pressure;
+        Emit?.Invoke(value);
     }
 
     public event Action<IDeviceReport>? Emit;
     public PipelinePosition Position => PipelinePosition.PreTransform;
-
-    [Property("X coordinate polynomial"), DefaultPropertyValue("x"), ToolTip(
-         "A polynomial that calculates the X coordinate\n" + TOOLTIP)]
-    public string XFunc { get; set; }
-
-    [Property("Y coordinate polynomial"), DefaultPropertyValue("y"), ToolTip(
-         "A polynomial that calculates the Y coordinate\n" + TOOLTIP)]
-    public string YFunc { get; set; }
-
-    [Property("Pressure polynomial"), DefaultPropertyValue("p"), ToolTip(
-         "A polynomial that calculates the pressure\n" + TOOLTIP)]
-    public string PFunc { get; set; }
-
-    [Property("X tilt polynomial"), DefaultPropertyValue("tx"), ToolTip(
-         "A polynomial that calculates the X tilt\n" + TOOLTIP)]
-    public string TXFunc { get; set; }
-
-    [Property("Y tilt polynomial"), DefaultPropertyValue("ty"), ToolTip(
-         "A polynomial that calculates the Y tilt\n" + TOOLTIP)]
-    public string TYFunc { get; set; }
-
-    [TabletReference]
-    public TabletReference TabletReference { get; set; }
+    
 }
